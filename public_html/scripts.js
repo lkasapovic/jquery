e557a630608bec15b47eeef771e41c7f6da25738
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2015-01-13 09:16:19 -0800

    @1van5algado #mexican/black

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -1,29 +1,23 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-$("document").ready(function(){
+$("document").ready(function() {
     $('.lana').css("background-color", "cyan");
-    $('.div p:nth-child(first)').css('background-color','red');
-    $("h1").bind('mouseover',mouseOverMe).bind('mouseout',mouseOutMe);
+    $('.div p:nth-child(first)').css('background-color', 'red');
+    $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
 });
 
 
-function mouseOverMe(){
+function mouseOverMe() {
     $("h1").html("MouseOn");
 }
 
-function mouseOutMe(){
+function mouseOutMe() {
     $("h1").html("MouseOff");
 }
 
 $("document").ready(function() {
-    
-    $('#replaceWHtml').bind('click',replaceWHtml);
+
+    $('#replaceWHtml').bind('click', replaceWHtml);
     $('#replaceWText').bind('click', replaceWText);
-    
+
 });
 
 function replaceWHtml() {
