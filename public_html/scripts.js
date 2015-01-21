892b4dd6562900c37adbf2c173dba0077d841d94
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2015-01-21 09:20:53 -0800

    no message

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -9,9 +9,16 @@ $("document").ready(function() {
     
      $('#show').css('visibility','hidden');
      
+     $('#accordion').accordion({header: "h4"});
      
-    
-    $('#accordian').accordion({header: "h4"});
+     
+     $('#replaceWText').bind('click', replaceWText);
+     
+     $('#addAPara').bind('click', addAPara);
+     
+     $('#randPara').bind('click', addAPara);
+     
+     $('#removeAPara').bind('click', removeAPara);
 });
 
 function hideThePage(){
@@ -24,4 +31,27 @@ function showThePage(){
     $('div').hide('fold', {}, 2500);
     $('#show').show('puff', {}, 2500);
 }
+
+function mouseOverMe(){
+    $("h1").html("HI");
+}
+
+function mouseOutMe(){
+    $("h1").text("LEJLA KASAPOVIC");
+}
  
+ function changeToText(){
+     $(".h1").text("<h6>BYE</h6>");
+ }
+ 
+ function replaceWText(){
+     $('#randPara').html('<h6>BOSNIA</h6>');
+ }
+ 
+ function addAPara(){
+     $('#randPara').append('<p>SOPHIA BUSH</p>')
+ }
+ 
+ function removeAPara(){
+     $('#randPara p:last').remove();
+ }
